3dd8cbaab06f010352ef5fb4e360023c39be13e6
yyokoo <revaldan1357@gmail.com>
2023-02-10 16:42:32 +0900

    validatesの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
+  
 end
